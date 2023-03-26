157ca523bce2485071700e339c74b7fb5b25f7f8
Mihail Galkin <galkin.mihail.11.06@yandex.ru>
2023-03-26 12:46:06 +0300

    verify = False !!DANGER!!

--- app/diary_class.py
+++ app/diary_class.py
@@ -66,7 +66,7 @@ class Diary:
         self.session = requests.Session()
         self.session.trust_env = False
         self.session.headers.update(get_header())
-        self.session.get("https://one.43edu.ru/", cookies=cookies, proxies={os.getenv("proxy1"): os.getenv("proxy2")}, verify="43edu-ru-chain.pem")
+        self.session.get("https://one.43edu.ru/", cookies=cookies, proxies={os.getenv("proxy1"): os.getenv("proxy2")}, verify=False)
 
         self.weeks: typing.Dict[d, Week] = {}
 
